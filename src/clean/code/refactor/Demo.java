d06d1677242c0feeefc1eb48ea659ac300bbc7dc
phamvanoanh <oanhmrphamvan@gmail.com>
2020-02-09 22:15:01 +0700

    add refactor code

---
+++ src/clean/code/refactor/Demo.java
@@ -0,0 +1,21 @@
+package clean.code.refactor;
+
+import clean.code.vo.Apple;
+
+import java.util.function.Supplier;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Apple ap1 = new Apple();
+        String name = getAppleName(ap1);
+
+        Supplier<Apple> sap1 = Apple::new;
+        String name1 = getAppleName(sap1.get());
+    }
+
+    private static String getAppleName (Apple apple){
+        return apple.getName();
+    }
+
+}
